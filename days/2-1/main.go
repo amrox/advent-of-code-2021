4d9d2a2843c1139d6694f2e7cb4ab94ad1e171a1
Andy Mroczkowski <a@mrox.co>
2021-12-04 17:20:40 -0500

    day 2-1

---
+++ days/2-1/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	log.Println("aoc 2-1")
+
+	fileArg := os.Args[1]
+
+	// os.Open() opens specific file in
+	// read-only mode and this return
+	// a pointer of type os.
+	file, err := os.Open(fileArg)
+
+	if err != nil {
+		log.Fatalf("failed to open")
+	}
+
+	// The bufio.NewScanner() function is called in which the
+	// object os.File passed as its parameter and this returns a
+	// object bufio.Scanner which is further used on the
+	// bufio.Scanner.Split() method.
+	scanner := bufio.NewScanner(file)
+
+	// The bufio.ScanLines is used as an
+	// input to the method bufio.Scanner.Split()
+	// and then the scanning forwards to each
+	// new line using the bufio.Scanner.Scan()
+	// method.
+	scanner.Split(bufio.ScanLines)
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	// The method os.File.Close() is called
+	// on the os.File object to close the file
+	file.Close()
+
+	// and then a loop iterates through
+	// and prints each of the slice values
+
+	var hPos = 0
+	var depth = 0
+	for _, each_ln := range text {
+		s := strings.Fields(each_ln)
+		dir := s[0]
+		val, _ := strconv.Atoi(s[1])
+		fmt.Println(s)
+		switch dir {
+		case "forward":
+			hPos += val
+		case "down":
+			depth += val
+		case "up":
+			depth -= val
+		}
+	}
+	fmt.Println(hPos * depth)
+}
